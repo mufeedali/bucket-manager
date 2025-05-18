7c5c5de74ae06bd7d361500448adae4ba353d119
Mufeed Ali <me@mufeed.dev>
2025-05-18 16:46:54 +0000

    feat: some colors, set tailwindcss association

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
           defaultTheme="dark"
           enableSystem={true}
           disableTransitionOnChange
+          themes={['light', 'dark']}
         >
           {children}
         </ThemeProvider>
